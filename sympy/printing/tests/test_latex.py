5b5f6f5871615dfbb99f76cf483a3da6894ac963
Tom Bachmann <e_mc_h2@web.de>
2011-07-21 22:29:14 +0100

    Add lerchphi, polylog.
    This commit adds lerch phi and polylog functions. In particular:
    
    Lerch transcendent:
    - extensive docstring
    - numerical evaluation
    - differentiation w.r.t. a
    - expansion of a=1 or z=0,1,-1
    - tests
    
    Polylogarithm:
    - docstring
    - numerical evaluation
    - differentiation w.r.t. z
    - rewriting as lerchphi
    - expansion for parameter 1, 0, negative integer
    - tests
    
    Also adds rewriting of hurwitz zeta as lerch phi.

--- sympy/printing/tests/test_latex.py
+++ sympy/printing/tests/test_latex.py
@@ -3,7 +3,7 @@ from sympy import (symbols, Rational, Symbol, Integral, log, diff, sin, exp,
     Order, Piecewise, Matrix, asin, Interval, EmptySet, Union, S, Sum,
     Limit, oo, Poly, Float, lowergamma, uppergamma, hyper, meijerg,
     Lambda, Poly, RootOf, RootSum, sqrt, Dict, catalan,
-    cot, coth, re, im, root, arg, zeta, dirichlet_eta)
+    cot, coth, re, im, root, arg, zeta, dirichlet_eta, polylog, lerchphi)
 from sympy.abc import mu, tau
 from sympy.printing.latex import latex
 from sympy.utilities.pytest import XFAIL, raises
@@ -149,6 +149,10 @@ def test_latex_functions():
     assert latex(zeta(x, y)**2) == r"\zeta^{2}\left(x, y\right)"
     assert latex(dirichlet_eta(x)) == r"\eta\left(x\right)"
     assert latex(dirichlet_eta(x)**2) == r"\eta^{2}\left(x\right)"
+    assert latex(polylog(x, y)) == r"\operatorname{Li}_{x}\left(y\right)"
+    assert latex(polylog(x, y)**2) == r"\operatorname{Li}_{x}^{2}\left(y\right)"
+    assert latex(lerchphi(x, y, n)) == r"\Phi\left(x, y, n\right)"
+    assert latex(lerchphi(x, y, n)**2) == r"\Phi^{2}\left(x, y, n\right)"
 
 def test_hyper_printing():
     from sympy import pi, Tuple
