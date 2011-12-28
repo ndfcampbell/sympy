c829b1bfad373323696731ecb37a493bacb70a3d
Matthew Rocklin <mrocklin@gmail.com>
2011-12-28 09:23:04 -0800

    sympifying argument in Normal so is_Matrix works on numeric inputs

--- sympy/stats/mvnrv_examples.py
+++ sympy/stats/mvnrv_examples.py
@@ -1,11 +1,12 @@
 from mvnrv import SingleMultivariatePSpace
 from crv_examples import Normal as SingleNormal
+from sympy import sympify
 
 def MultivariateNormal(mean, covariance, symbol=None):
     return SingleMultivariatePSpace(mean, covariance, symbol).value
 
 def Normal(mean, spread, symbol=None):
-    if mean.is_Matrix:
+    if sympify(mean).is_Matrix:
         return MultivariateNormal(mean, spread, symbol)
     else:
         return SingleNormal(mean, spread, symbol)
