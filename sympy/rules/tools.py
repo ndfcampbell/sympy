447e45b08944be869aa85a1aa36809bbdb19c5de
Matthew Rocklin <mrocklin@gmail.com>
2012-10-25 12:03:12 -0700

    add subs rule and tool
    replace hashing with enumeration in rules subs

---
+++ sympy/rules/tools.py
@@ -0,0 +1,14 @@
+import rl
+from strat import canon, do_one, chain
+
+def subs(d):
+    """ Full simultaneous exact substitution """
+    from sympy import Integer
+    # TODO: Find a better way to ensure that we don't accidentaly match
+    enum = {v: 2942742429+i for i,v in enumerate(d.values())}
+    step1 = {k: Integer(enum[v]) for k,v in d.items()}
+    step2 = {Integer(enum[v]): v for k,v in d.items()}
+    subs1 = do_one(*map(rl.subs, *zip(*step1.items())))
+    subs2 = do_one(*map(rl.subs, *zip(*step2.items())))
+
+    return chain(canon(subs1), canon(subs2))
