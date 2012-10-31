1ad1fc58e258b64ac2683168bd0517a53a627dca
Matthew Rocklin <mrocklin@gmail.com>
2012-10-31 11:09:37 -0500

    remove Integer dependence in rules.tools.subs

--- sympy/rules/tools.py
+++ sympy/rules/tools.py
@@ -3,11 +3,10 @@ from strat import canon, do_one, chain
 
 def subs(d):
     """ Full simultaneous exact substitution """
-    from sympy import Integer
     # TODO: Find a better way to ensure that we don't accidentaly match
     enum = {v: 2942742429+i for i,v in enumerate(d.values())}
-    step1 = {k: Integer(enum[v]) for k,v in d.items()}
-    step2 = {Integer(enum[v]): v for k,v in d.items()}
+    step1 = {k: enum[v] for k,v in d.items()}
+    step2 = {enum[v]: v for k,v in d.items()}
     subs1 = do_one(*map(rl.subs, *zip(*step1.items())))
     subs2 = do_one(*map(rl.subs, *zip(*step2.items())))
 
