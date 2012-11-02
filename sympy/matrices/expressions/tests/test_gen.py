528c763d8dd34323387a91362c180738071091d0
Matthew Rocklin <mrocklin@gmail.com>
2012-11-02 13:26:10 -0500

    stronger test for generation

--- sympy/matrices/expressions/tests/test_gen.py
+++ sympy/matrices/expressions/tests/test_gen.py
@@ -1,5 +1,5 @@
 from sympy.matrices.expressions.gen import blas_rule
-from sympy.matrices.expressions.blas import TRSV
+from sympy.matrices.expressions.blas import TRSV, GEMM
 from sympy.matrices.expressions import MatrixSymbol
 
 def test_blas_rule():
@@ -9,4 +9,7 @@ def test_blas_rule():
     x = MatrixSymbol('x', 3, 1)
 
     expr = (3*X*Y + 2*Z).I*x
-    assert isinstance(blas_rule(expr).next(), TRSV)
+    comp = blas_rule(expr).next()
+    assert isinstance(comp, TRSV)
+    comp2 = blas_rule(comp.inputs[0]).next()
+    assert isinstance(comp2, GEMM)
