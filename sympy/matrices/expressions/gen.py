9b6adfc3ac21e5d8e3976cbfa6fe9a9c73fa8678
Matthew Rocklin <mrocklin@gmail.com>
2012-11-04 07:31:31 -0600

    tmp - traverse rewrite blas rules

--- sympy/matrices/expressions/gen.py
+++ sympy/matrices/expressions/gen.py
@@ -1,13 +1,49 @@
-from sympy.matrices.expressions.blas import GEMM, TRSV
+from sympy.matrices.expressions.blas import GEMM, TRSV, SYMM
 from sympy.unify.unify_sympy import unify, patternify
 from sympy.unify.rewrite import rewriterule
-from sympy.rules.branch import multiplex
-from sympy import MatrixSymbol
+from sympy.rules.branch import multiplex, condition
+from sympy import MatrixSymbol, MatrixExpr
+from sympy import ask, Q
 
-def rr_from_blas(cls):
+def rr_from_blas(cls, assumptions=True):
     pattern_in = patternify(cls._outputs[0], *cls._inputs)
     pattern_out = cls(*cls._inputs)
-    return rewriterule(pattern_in, pattern_out)
+    rr = rewriterule(pattern_in, pattern_out)
+    def blas_brl(expr):
+        for blas in rr(expr):
+            if cls.valid(blas.inputs, assumptions):
+                yield blas
+    return blas_brl
 
-blas_rules = map(rr_from_blas, (GEMM, TRSV))
-blas_rule = multiplex(*blas_rules)
+
+classes = (GEMM, TRSV, SYMM)
+def build_rule(assumptions, classes=classes):
+    return multiplex(*[rr_from_blas(cls, assumptions) for cls in classes])
+
+# blas_rules = map(rr_from_blas, (GEMM, TRSV))
+# blas_rule = multiplex(*blas_rules)
+
+from sympy.rules.branch.strat_pure import notempty
+from sympy.core.compatibility import product
+from sympy.computations import Computation
+
+new = lambda ne, *cs: ne._composite(cs)
+is_leaf = lambda x: not isinstance(x, Computation)
+children = lambda x: x.inputs
+
+def top_down(brule):
+    """ Apply a rule down a tree running it on the top nodes first """
+    ident = "_identity"
+    def top_down_rl(expr):
+        print "In:  ", expr
+        anything = False
+        for comp in brule(expr):
+            anything = True
+            for comps in product(*map(top_down_rl, comp.inputs)):
+                x = comp._composite(filter(lambda x: not x is ident, comps) +
+                                      (comp,))
+                print "Out: ", x
+                yield x
+        if not anything:
+            yield ident
+    return top_down_rl
--- sympy/matrices/expressions/tests/test_gen.py
+++ sympy/matrices/expressions/tests/test_gen.py
@@ -1,15 +1,53 @@
-from sympy.matrices.expressions.gen import blas_rule
-from sympy.matrices.expressions.blas import TRSV, GEMM
+from sympy.matrices.expressions.gen import build_rule, top_down
+# from sympy.matrices.expressions.gen import blas_rule
+from sympy.matrices.expressions.blas import TRSV, GEMM, SYMM
 from sympy.matrices.expressions import MatrixSymbol
+from sympy import Q
+
+X = MatrixSymbol('X', 3, 3)
+Y = MatrixSymbol('Y', 3, 3)
+Z = MatrixSymbol('Z', 3, 3)
+x = MatrixSymbol('x', 3, 1)
+
+def test_rr_from_blas():
+    assumptions = (Q.lower_triangular(X) & Q.lower_triangular(Y) &
+                   Q.lower_triangular(Z))
+    expr = (3*X*Y + 2*Z).I*x
+    rr = rr_from_blas(TRSV, assumptions)
+    assert rr(expr).next() == TRSV(3*X*Y + 2*Z, x)
+
+    rr = rr_from_blas(TRSV)
+    assert len(rr(expr)) == 0
 
 def test_blas_rule():
-    X = MatrixSymbol('X', 3, 3)
-    Y = MatrixSymbol('Y', 3, 3)
-    Z = MatrixSymbol('Z', 3, 3)
-    x = MatrixSymbol('x', 3, 1)
+    assumptions = (Q.lower_triangular(X) & Q.lower_triangular(Y) &
+                   Q.lower_triangular(Z))
+    translate = build_rule(assumptions)
+
+    assert list(translate(2*Z+3*X*Y)) == [GEMM(3, X, Y, 2, Z)]
+
 
     expr = (3*X*Y + 2*Z).I*x
     comp = blas_rule(expr).next()
     assert isinstance(comp, TRSV)
     comp2 = blas_rule(comp.inputs[0]).next()
     assert isinstance(comp2, GEMM)
+
+def test_multiple_outs():
+    expr = (3*X*Y + 2*Z)
+    assumptions = Q.symmetric(X)
+    blas_rule = build_rule(assumptions)
+    assert set(map(type, blas_rule(expr))) == {SYMM, GEMM}
+
+    assumptions = True
+    blas_rule = build_rule(assumptions)
+    assert set(map(type, blas_rule(expr))) == {GEMM}
+
+def test_traverse():
+    assumptions = (Q.lower_triangular(3*X*Y+2*Z) & Q.symmetric(Y))
+    expr = (3*X*Y + 2*Z).I*x
+    blas_rule = top_down(build_rule(assumptions))
+    return blas_rule(expr)
+    comp = blas_rule(expr).next()
+    return comp
+    assert False
