4836639b2af8394cbf70999a91a8717e8162d514
Matthew Rocklin <mrocklin@gmail.com>
2012-11-08 12:01:42 -0600

    breakup gesv test

--- sympy/matrices/expressions/tests/test_lapack.py
+++ sympy/matrices/expressions/tests/test_lapack.py
@@ -7,17 +7,20 @@ from sympy.matrices.expressions.lapack import *
 n,m,k = symbols('n,m,k')
 C = MatrixSymbol('C', n, k)
 S = MatrixSymbol('S', n, n)
+A = MatrixSymbol('A', n, n)
+B = MatrixSymbol('B', n, k)
+gesv = GESV(A, B)
 
 def test_GESV():
-    A = MatrixSymbol('A', n, n)
-    B = MatrixSymbol('B', n, k)
-    gesv = GESV(A, B)
     assert gesv.inputs  == (A, B)
     assert gesv.outputs[0] == A.I*B
+
+def test_in_out_types():
     assert gesv.in_types == ('real*8', 'real*8')
     assert gesv.out_types == ('real*8', 'integer', 'integer')
     assert gesv.types()[gesv.outputs[1]] == 'integer'
 
+def test_ipiv_header_declaration():
     assert 'integer, intent(out) :: IPIV(n)' in gesv.print_Fortran(str)
 
 def test_types():
