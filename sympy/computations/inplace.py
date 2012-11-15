35fca21af108ab0b3d85142067b5410d5e71b56c
Matthew Rocklin <mrocklin@gmail.com>
2012-11-15 16:44:48 -0600

    add inplace file and getname function

---
+++ sympy/computations/inplace.py
@@ -0,0 +1,27 @@
+
+def make_getname():
+    cache = {}
+    seen = set(['', None])
+
+    def getname(key, requested=None):
+        """ Get the name associated to a key """
+        if key in cache:
+            return cache[key]
+
+        name = requested
+        if not name and hasattr(key, 'name'):
+            name = key.name
+        else:
+            name = ''
+        if name in seen:
+            id = 2
+            while(name + '_%d'%id in seen):
+                id += 1
+            name = name + '_%d'%id
+
+        cache[key] = name
+        seen.add(name)
+        return name
+    return getname
+
+
