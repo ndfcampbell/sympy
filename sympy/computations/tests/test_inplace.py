e63ce517e604492116374aa91421d780c35cff2c
Matthew Rocklin <mrocklin@gmail.com>
2012-11-15 16:50:02 -0600

    add Copy class

--- sympy/computations/tests/test_inplace.py
+++ sympy/computations/tests/test_inplace.py
@@ -1,4 +1,4 @@
-from sympy.computations.inplace import make_getname
+from sympy.computations.inplace import make_getname, Copy
 from sympy import Symbol
 
 def test_getname():
@@ -8,3 +8,8 @@ def test_getname():
     assert getname(Symbol('y')) == 'y'
     assert getname(Symbol('x', real=True)) == 'x_2'
     assert len(set(map(getname, (1, 2, 2, 2, 3, 3, 4)))) == 4
+
+def test_copy():
+    c = Copy(Symbol('x'))
+    assert c.name == 'x'
+
