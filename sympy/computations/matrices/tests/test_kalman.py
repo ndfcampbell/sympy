e0a67df634c5c7052fe9798ec54a6cc42a44326b
Matthew Rocklin <mrocklin@gmail.com>
2012-12-13 10:33:21 -0600

    add numpy kalman test

--- sympy/computations/matrices/tests/test_kalman.py
+++ sympy/computations/matrices/tests/test_kalman.py
@@ -15,3 +15,21 @@ def test_fortran_code_generation():
     except:
         ic.show()
     assert callable(f)
+
+def test_kalman_run():
+    ic = inplace_compile(mathcomp, Copy=COPY)
+    f = build(ic, assumptions, 'kalman', input_order=(mu, Sigma, H, R, data))
+    try:
+        n, m, k = 50, 40, 30
+        alpha, beta = 2.5, 3.2
+        import numpy as np
+        nSigma = np.array(np.random.rand(n, n), order='F')
+        nI = np.array(np.eye(n), order='F')
+        nmu = np.array(np.random.rand(n), order='F')
+        nH = np.array(np.random.rand(k, n), order='F')
+        nR = np.array(np.random.rand(k, k), order='F')
+        ndata = np.array(np.random.rand(k), order='F')
+        output = f(nmu, nSigma, nH, nI, nR, ndata)
+        print output
+    except ImportError:
+        pass
