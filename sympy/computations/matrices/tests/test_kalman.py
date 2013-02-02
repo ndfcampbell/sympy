006f65d98031e21db32421d905c97025eda8e7d7
Matthew Rocklin <mrocklin@gmail.com>
2013-02-02 10:34:00 -0800

    remove identity from kalman test

--- sympy/computations/matrices/tests/test_kalman.py
+++ sympy/computations/matrices/tests/test_kalman.py
@@ -23,12 +23,11 @@ def test_kalman_run():
         alpha, beta = 2.5, 3.2
         import numpy as np
         nSigma = np.array(np.random.rand(n, n), order='F')
-        nI = np.array(np.eye(n), order='F')
         nmu = np.array(np.random.rand(n), order='F')
         nH = np.array(np.random.rand(k, n), order='F')
         nR = np.array(np.random.rand(k, k), order='F')
         ndata = np.array(np.random.rand(k), order='F')
-        output = f(nmu, nSigma, nH, nI, nR, ndata)
+        output = f(nmu, nSigma, nH, nR, ndata)
         print output
     except ImportError:
         pass
