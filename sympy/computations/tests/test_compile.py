d82be98acb93cdb94539e8d09fe339fb9136504c
Matthew Rocklin <mrocklin@gmail.com>
2013-02-01 17:10:59 -0800

    fix tests in computations/compile
    variables were mixing with inputs

--- sympy/computations/tests/test_compile.py
+++ sympy/computations/tests/test_compile.py
@@ -3,13 +3,12 @@ from sympy.computations.compile import (input_crunch, output_crunch,
         multi_input_rule, multi_output_rule)
 from sympy.rules.branch import multiplex, exhaust, debug
 from sympy.computations.core import Identity
-from sympy import Symbol, symbols, S
+from sympy import Symbol, symbols, S, Dummy
 from sympy.unify import unify, rewriterule, rebuild
+from sympy.abc import a, b, c, x, y, z
 
 rules = map(rewriterule, *zip(*patterns))
 rule = exhaust(multiplex(*map(input_crunch, rules)))
-
-x, y, z = symbols('x,y,z')
 
 def test_compile():
     expr = y + 1
@@ -26,32 +25,33 @@ def test_input_crunch():
     assert len(list(rule(comp))) == 2
 
 def test_add():
-    expr = y + 3
+    expr = z + 3
     rule = rewriterule(x + y, add(x, y), [x, y])
-    assert set(rule(expr)) == set([add(S(3), y), add(y, S(3))])
+    assert set(rule(expr)) == set([add(S(3), z), add(z, S(3))])
     rule = multiplex(*map(input_crunch, rules))
     comp = Identity(expr)
-    assert set(rule(comp)) == set([add(S(3), y), add(y, S(3))])
+    assert set(rule(comp)) == set([add(S(3), z), add(z, S(3))])
 
 def test_rule():
-    y = Symbol('y')
-    expr = 2*y + 1
+    z = Symbol('z')
+    expr = 2*z + 1
     comp = Identity(expr)
-    assert set(rule(comp)) == set([add(2*y, S(1)) + double(y),
-                                   add(S(1), 2*y) + double(y),
-                                   inc(2*y) + double(y)])
+    assert set(rule(comp)) == set([add(2*z, S(1)) + double(z),
+                                   add(S(1), 2*z) + double(z),
+                                   inc(2*z) + double(z)])
 
 def _test_multi_rule(multi_rule):
-    expr = y + 1, y - 1
+    expr = z + 1, z - 1
     comp = Identity(*expr)
     rule = multi_rule((x + 1, x - 1), incdec(x), x)
-    assert list(rule(comp)) == [incdec(y)]
+    assert list(rule(comp)) == [incdec(z)]
 
 def _test_multi_with_extra_inputs(multi_rule):
-    expr = y + 1, y - 1, x + 1
+    expr = a + 1, a - 1, b + 1
     comp = Identity(*expr)
-    rule = multi_rule((x + 1, x - 1), incdec(x), x)
-    assert list(rule(comp)) == [incdec(y) + Identity(x+1) ]
+    rule = multi_rule((c + 1, c - 1), incdec(c), c)
+    print list(rule(comp))
+    assert list(rule(comp)) == [incdec(a) + Identity(b + 1) ]
 
 def test_multi_input():
     _test_multi_rule(multi_input_rule)
