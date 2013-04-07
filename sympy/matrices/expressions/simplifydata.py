a2aa8b735f1dafa1904ca387855a4467262a00fc
Matthew Rocklin <mrocklin@gmail.com>
2013-04-07 12:45:54 -0500

    spacing

--- sympy/matrices/expressions/simplifydata.py
+++ sympy/matrices/expressions/simplifydata.py
@@ -19,19 +19,27 @@ vars = [A, B, n, m, Sq, SA, SB, SC, SD]
 
 known_relations = [
     (A.T, A, Q.symmetric(A)),
+
     (Sq.I, Sq.T, Q.orthogonal(Sq)),
 
     # Determinants
     (det(Sq), S.Zero, Q.singular(Sq)),
-    (det(BlockMatrix([[SA,SB],[SC,SD]])), det(SA)*det(SD - SC*SA.I*SB),
+
+    (det(BlockMatrix([[SA,SB],[SC,SD]])),
+        det(SA)*det(SD - SC*SA.I*SB),
         Q.invertible(SA)),
-    (det(BlockMatrix([[SA,SB],[SC,SD]])), det(SD)*det(SA - SB*SD.I*SA),
+
+    (det(BlockMatrix([[SA,SB],[SC,SD]])),
+        det(SD)*det(SA - SB*SD.I*SA),
         Q.invertible(SD)),
+
     (det(SA), S.One, Q.orthogonal(SA)),
+
     (Abs(det(SA)), S.One, Q.unitary(SA)),
 
     # BlockMatrices
     (BlockMatrix([[A]]), A, True),
+
     (Inverse(BlockMatrix([[SA, SB],
                           [SC, SD]])),
      BlockMatrix([[ (SA - SB*SD.I*SC).I,  (-SA).I*SB*(SD - SC*SA.I*SB).I],
